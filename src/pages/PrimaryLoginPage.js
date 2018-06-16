ad0450fe5c30bf4de792eaf1c7ea76f4573ffcfb
IsabelaBatarce <isabelabatarce15@gmail.com>
2018-06-15 20:08:45 -0400

    novo component
    criacao do formrow e modificacao no primarylogin

--- src/pages/PrimaryLoginPage.js
+++ src/pages/PrimaryLoginPage.js
@@ -1,11 +1,61 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput,Text, StyleSheet, Button } from 'react-native';
+import FormRow from'../components/FormRow';
 
-export default class PrimaryLoginPage extends React.Component {
+export default class PrimaryLoginPage extends React.Component {    
     render(){
         return(
-            <Text> pg </Text>
+            <View style= { styles.container }>
+					
+					<FormRow first>
+						
+						
+							
+							<TextInput 
+								style={ styles.input }
+								placeholder= "Nome"
+								
+								
+
+							/>
+					
+					</FormRow>
+
+					<FormRow last>
+		
+							<TextInput 
+								style={ styles.input}
+								placeholder= "*******"
+								secureTextEntry 
+								
+								
+							/>
+					
+					</FormRow>
+
+					<Button 
+						title = "Entrar"
+						onPress={() => this.tryLogin()}
+					 />
+
+				</View>	
         )
     }
 
 }
+
+const styles = StyleSheet.create({
+	container:{
+		paddingLeft: 10,
+		paddingRight: 10
+	},
+	
+	input: {
+		paddingLeft: 5,
+		paddingRight: 5,
+		paddingBottom: 5
+	}
+	
+
+})
+
