071ed75b8ee0aaa36798d50442bde3de6cf3563a
IsabelaBatarce <isabelabatarce15@gmail.com>
2018-06-15 21:34:28 -0400

    modificacao no list select

--- src/pages/PrimaryLoginPage.js
+++ src/pages/PrimaryLoginPage.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import { View, TextInput,Text, StyleSheet, Button } from 'react-native';
+import { View, TextInput,Text, StyleSheet, Button, TouchableHighlight } from 'react-native';
 import FormRow from'../components/FormRow';
 
+import ListPopover from 'react-native-list-popover';
+
+const items = ['Item 1', 'Item 2'];
+
 export default class PrimaryLoginPage extends React.Component {    
+    
+    constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: false
+    };
+  }
+
     render(){
         return(
-            <View style= { styles.container }>
-					
-					<FormRow first>
-						
-						
-							
-							<TextInput 
-								style={ styles.input }
-								placeholder= "Nome"
-								
-								
+      
+      <View style = { styles.oi }>			
+        
+        <TouchableHighlight
+          
+          style={styles.button}
+          onPress={() => this.setState({isVisible: true})}>
+          
+          <Text>{this.state.item || 'Select'}</Text>
+        
+        </TouchableHighlight>
+        
+        <ListPopover
+          
+          list={items}
+          isVisible={this.state.isVisible}
+          onClick={(item) => this.setState({item: item})}
+          onClose={() => this.setState({isVisible: false})}/>
+      	
 
-							/>
-					
-					</FormRow>
+            <View style= { styles.container }>
+
+					      
+        	
+      	</View>
 
 					<FormRow last>
 		
@@ -54,7 +76,21 @@ const styles = StyleSheet.create({
 		paddingLeft: 5,
 		paddingRight: 5,
 		paddingBottom: 5
-	}
+	},
+	oi: {
+ 
+    alignItems: 'center',
+   
+    justifyContent: 'center',
+    marginTop: 50
+  },
+  button: {
+    backgroundColor: '#b8c',
+    borderRadius: 4,
+   
+    padding: 10,
+
+  }
 	
 
 })
--- src/pages/SecondaryLoginPage.js
+++ src/pages/SecondaryLoginPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default class SecondaryLoginPage extends React.Component {
+export default class SyncPage extends React.Component {
     render(){
         return(
             <Text> pg </Text>
